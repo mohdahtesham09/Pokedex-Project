c6ecb28e29b49e3f6118130ec1963b9f0845ad88
mohdahtesham09 <ahteshammohd094@gmail.com>
2025-07-14 18:02:08 +0530

    Adding css animation and voice

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,11 +1,27 @@
 import './pokemon.css'
 
 function Pokemon({ name , image}){
+
+     // ✅ Speak function
+  const speakName = () => {
+    const utterance = new SpeechSynthesisUtterance(name);
+    utterance.lang = 'en-IN';
+    speechSynthesis.speak(utterance);
+  };
+    
+
     return(
-        <div className='pokemon'>
-            <div className='pokemon-name'>{name}</div>
-            <div><img className='pokemon-image' src={image}/></div>
-        </div>
+         <div className='pokemon' onClick={speakName}>
+      <div className='pokemon-name'>{name}</div>
+      <div>
+        <img className='pokemon-image' src={image} alt={name} />
+      </div>
+    </div>
+        
+        // <div className='pokemon'>
+        //     <div className='pokemon-name'>{name}</div>
+        //     <div><img className='pokemon-image' src={image}/></div>
+        // </div>
     )
 
 }
